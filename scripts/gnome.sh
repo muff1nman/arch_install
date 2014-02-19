3ebb937055d16a0ce7743d82d05b89fc6ac87e36
Andrew DeMaria <ademaria@mines.edu>
2014-02-18 22:23:39 -0700

    Another dwm

---
+++ scripts/gnome.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# gnome.sh
+# Copyright (C) 2014 Andrew DeMaria (muff1nman) <ademaria@mines.edu>
+#
+# All Rights Reserved.
+#
+
+
+yaourt -S gnome-shell gnome gnome-extra
+# TODO configure xinitrc
+
